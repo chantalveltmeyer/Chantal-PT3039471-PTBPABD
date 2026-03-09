e96ae63aac76db9dde4a16ac68c6d2483ab282fb
chantalveltmeyer <chantal.veltmeyer@aluno.ifsp.edu.br>
2026-03-09 20:25:44 -0300

    Add files via upload

---
+++ Consultas.sql
@@ -0,0 +1,21 @@
+SELECT COUNT(*) AS quantidade_registros FROM advisor;
+
+SELECT COUNT(*) AS quantidade_registros FROM classroom;
+
+SELECT COUNT(*) AS quantidade_registros FROM course;
+
+SELECT COUNT(*) AS quantidade_registros FROM department;
+
+SELECT COUNT(*) AS quantidade_registros FROM instructor;
+
+SELECT COUNT(*) AS quantidade_registros FROM prereq;
+
+SELECT COUNT(*) AS quantidade_registros FROM section;
+
+SELECT COUNT(*) AS quantidade_registros FROM student;
+
+SELECT COUNT(*) AS quantidade_registros FROM takes;
+
+SELECT COUNT(*) AS quantidade_registros FROM teaches;
+
+SELECT COUNT(*) AS quantidade_registros FROM time_slot;
